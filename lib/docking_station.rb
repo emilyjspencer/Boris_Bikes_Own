5b44a7bd5b6217b0aaf1cfd33b3b9a5a7b85fd91
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-14 18:10:01 +0000

    Can dock a bike at the dock station and. Docked bike stored in an instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,15 @@
+require_relative 'bike'
+
 class DockingStation
+    attr_reader :bike
   def release_bike(bike)
   end 
+
+  def dock(bike)
+    @bike = bike
+  end 
+
+
+  
+  
 end 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,30 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do 
+  describe '#release_bike' do
+    it { is_expected.to respond_to(:release_bike).with(1).argument }
+    
+    it 'releases a working bike' do
+      bike = Bike.new
+      subject.release_bike(bike)
+      expect(bike).to be_working
+    end 
+  end
 
-  it { is_expected.to respond_to(:release_bike).with(1).argument }
-  # Add a test to your DockingStation specification that a) gets a bike, and then b) expects the bike to be working
-  it 'releases a working bike' do
-    bike = Bike.new
-    subject.release_bike(bike)
-    expect(bike).to be_working
+  describe '#dock' do
+    it 'docks a bike at the docking station' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end 
+
+    it 'stores the bike that has been docked' do
+      bike = Bike.new 
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end 
   end 
+
+# As a member of the public
+# So I can decide whether to use the docking station
+# I want to see a bike that has been docked
 end 
